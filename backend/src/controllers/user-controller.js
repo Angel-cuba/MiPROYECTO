b517d13ad7373d339ccb91f2d3cc279f90e09796
Angel_Git_Lab <araozangel842@gmail.com>
2021-05-27 11:52:59 +0300

    Encriptado de password OK

--- backend/src/controllers/user-controller.js
+++ backend/src/controllers/user-controller.js
@@ -1,36 +1,41 @@
 const userCtrl = {}
 const dbConnection = require('../database/db')
-const dbConnections = require('../database/db')
 
+const bcrypt = require('bcrypt')
 
-userCtrl.newUser = (req, res) => {
-//     res.json('New User')
-          const user = req.body
+userCtrl.newUser = async(req, res) => {
 
-          const newUser = [
-               user.first_name,
-               user.last_name,
-               user.email,
-               user.pass
-          ]
+     //Recojo los datos del user
+      const user = req.body
+
+     //Encriptar el password
+     const encrypt = await bcrypt.genSalt(10)
+     const pass = await bcrypt.hash(req.body.pass, encrypt)
+
+          // Objeto de nuevo usuario, se iguala [pass(encriptada):pass]
+          const newUser ={
+              first_name:user.first_name,
+               last_name: user.last_name,
+              email: user.email,
+              pass: pass}
+          
      ////////////////////////////////  
-     if (!user.first_name || !user.last_name || !user.email ) {
+     if (!user.first_name || !user.last_name || !user.email ||!user.pass) {
         return res.json({
             ErrorCode: 204,
             Message: 'Fields cannot be empty'
         });
     }
+    
+          //Query
+      let sqlQuery = 'INSERT INTO users(first_name, last_name, email, pass) VALUES (?,?,?,?)'
 
 
-    let sqlQuery = 'INSERT INTO users (first_name, last_name, email, pass) VALUES (?,?,?,?)'
-
-    dbConnection.query(sqlQuery, newUser, (err, result) => {
+    dbConnection.query(sqlQuery, [newUser.first_name, newUser.last_name, newUser.email, newUser.pass], (err, result) => {
          if(err) throw err
 
          res.status(201).json('User inside database '+ result)
-
     })
-
 }
 
 userCtrl.updateUser = (req, res) => {
