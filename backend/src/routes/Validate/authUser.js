df3b92d4d6b50faadc88e5fd15bb863d1d9654d7
Angel_Git_Lab <araozangel842@gmail.com>
2021-05-27 15:53:55 +0300

    Validación de email y password

--- backend/src/routes/Validate/authUser.js
+++ backend/src/routes/Validate/authUser.js
@@ -1,25 +1,49 @@
 const router = require('express').Router()
 const dbConnection = require('../../database/db')
 
+const bcrypt = require('bcrypt')
+
 
 router.post('/login', async (req, res) => {
 
      const { email, pass } = req.body
  
+     let sqlQuery = 'SELECT * FROM users WHERE email = ? AND pass = ? '
 
-     let sqlQuery = `SELECT * FROM users WHERE email = ? AND pass = ?`
+  dbConnection.query(sqlQuery, [email, pass] ,(err, results) => {
+           if(err) {
+                res.send({          
+               "code":400,          
+               "failed":"error occurred",          
+               "error" : error        
+               })   
+                
+           }else {
+               
+                    if(results.length > 0) {
+                         const comparison = bcrypt.compare(pass, results[0].pass) 
+                              const checkingEmail = results[0].email
+                         // if(email != checkingEmail){
+                         //                               res.send({            
+                         //                                    "code":206,            
+                         //                                    "error":"Email does not exist"              
+                         //                                    });  
+                         //                               }
+                         if(comparison){
+                              res.send({                
+                                   "code":200,                
+                                   "success":"login successful",
+                         })
+                          }
+                         
+                 }                      
+               
 
-     dbConnection.query(sqlQuery, [email,pass] ,(err, results) => {
-
-          if (err) {
-               throw err
-          }
-          else {
-               res.status(200).json(results)
-          }
-     })
-
-
+                 else
+                     {    res.status(403).json("Email or password incorrect")
+                                             }    
+                         }
+               })
 })
 
 module.exports = router
