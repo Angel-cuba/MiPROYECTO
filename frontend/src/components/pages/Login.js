d8dab3abcd745b1b71adcc5a6c47d516118d322e
Angel <araozangel842@gmail.com>
2021-06-26 04:15:25 +0300

    Cleaning code and using Toastify

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,35 +1,30 @@
 import React from 'react'
-import { useHistory, Redirect } from 'react-router-dom'
-import '../../css/App.css';
-import { useState, useEffect, useContext} from 'react'
+import { useHistory} from 'react-router-dom'
+import './pagesCss/pages.css';
+import { useState, useContext} from 'react'
 // import {Route,  Redirect } from 'react-router-dom'
 import Axios from 'axios'
 import { AuthContext } from '../lib/auth.context'
-import EachUser from './EachUser'
-import NewLinks from '../NewLink'
-import All from '../../readLinks/All'
 
-
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 
  const Login = () => {
-    
+
+toast.configure()
+
  const setAuthContext = useContext(AuthContext)
 
  const [userEmail, setuserEmail] = useState("")
  const [userPassword, setuserPassword] = useState("")
-
-
- const [ LoggedUser, setLoggedUser] = useState("")
- const [ error, setError] = useState("")
+//  const [ LoggedUser, setLoggedUser] = useState("")
+ const [ error, setError] = useState()
 
   const history = useHistory()
 
+const login = (e,setAuth) => {
 
-
-
-
-const login = (e,setAuth, history) => {
    e.preventDefault();
 
    Axios.post('http://localhost:4000/api/user/login', {
@@ -39,49 +34,51 @@ const login = (e,setAuth, history) => {
    })
    .then(response =>{
        if(response.data.token){
-               console.log('test', response)
-           localStorage.setItem('userJWT', JSON.stringify(response.data));
+              // console.log('test', response)
+                const toastFx = () => {
+                        toast.success(`Hello ${response.data.userDB[0].first_name} is good to see ya `)
+                                 }
+           localStorage.setItem('jwt', JSON.stringify(response.data.token));
               setAuth(response.data.token)
-              // console.log(setAuth)
-              setLoggedUser(response.data.userDB)
-              console.log('user_name',response.data.userDB[0].first_name)
-
-              history.push('/')
-       }
+                              toastFx()
+                          
+               history.push('/EachUser')      
+       }  
    }).catch(( error )=> {
-     setError(error)
+    //  setError(error)
        console.log('este es el error : ', error)
    })
-
  } 
 
- 
-
-
  return (
-            <>
-   <div className="login">
+      <>
+            <div className="login_page">
+                 <div className="login">
       <h1>Login Page</h1>
-      <label>Username</label>
-      <input 
-      type="text"
-      id="email"
-      placeholder="Email address"
-      onChange={(e) => {setuserEmail(e.target.value)}}
-      />
-      <label>Password</label>
+      <div className="email">
+           <label>Username</label>
+            <input 
+            type="text"
+            id="email"
+            placeholder="Email address"
+            onChange={(e) => {setuserEmail(e.target.value)}}
+            autoFocus
+            />
+      </div>
+      <div className="password">
+            <label>Password</label>
       <input 
       type="text"
       id="password"
       placeholder="Password"
       onChange={(e) => {setuserPassword(e.target.value)}}
       autoComplete="off"/>
-           
-
-      <button onClick={(e) =>login(e, setAuthContext.setAuth)}> Login </button>
+      </div>
+      
+      <button className="btn_login" onClick={(e) =>{login(e, setAuthContext.setAuth)}}> Login </button>
     </div>
-  {error? (<div id="texto-error" className="error"></div>): ("")}
-    
+            </div>
+      
     </>
 
 
