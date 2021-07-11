a51f9ca33a0bf51e6542be66a1d5f043e2530611
Angel <araozangel842@gmail.com>
2021-07-11 21:45:40 +0300

    New Register page using Formik

---
+++ frontend/src/components/pages/UserRegister.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../pages/css/register.css';
+import { useHistory } from 'react-router-dom';
+
+//Importando Formik
+import { Formik, Form } from 'formik'
+import Input from '../../Small-Components/Input'
+
+import { formikSubmit } from '../lib/formikSubmit'
+
+
+const UserRegister = () => {
+// const btn = document.getElementById('btn')
+// if(!formikSubmit){
+//      btn.disabled = true
+// }
+
+     // btn.disabled = false
+
+ 
+ return (
+      <>
+      <div className="pages">
+           <Formik {...formikSubmit}>
+               {formik => (
+               <div className="register">
+                    <h1>Usando el Formik</h1>
+                         {/* {console.log(formik.values)} */}
+                         <Form>
+                              <Input label="First Name" name="first_name" placeholder="First Name" className="register_firstname"/>
+                              <Input label="Last Name" name="last_name" placeholder="Last Name" className="register_lastname"/>
+                              <Input label="Email" name="email" placeholder="Email" className="register_email"/>
+                              <Input label="Password" name="password" placeholder="Password" className =" register_password"/>
+                              <Input label="Confirm Password" name="confirmpassword" placeholder="Confirm your password" className =" register_password"/>
+                              {/* {formikSubmit ? () : ""} */}
+                              <button id="btn" type ="submit" className ="register_btn">Register</button>
+                          </Form>  
+                    </div> 
+                 ) 
+                   }              
+          </Formik>
+     </div>   
+     </>
+        
+     )
+}
+
+export default UserRegister
