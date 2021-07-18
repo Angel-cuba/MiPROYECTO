f162c98bee65eeeedeefc9972d583c1169645ff5
Angel <araozangel842@gmail.com>
2021-07-19 01:08:45 +0300

    Update One.js OK......All working

--- frontend/src/readLinks/One.js
+++ frontend/src/readLinks/One.js
@@ -1,46 +1,93 @@
 import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom'
 import './../components/pages/css/one.css'
+import { NavLink } from '../components/Navbar/NavbarElements'
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Formik, Form } from 'formik'
 import { formikUpdate } from '../lib/formikUpdate'
 
-import Input from '../Small-Components/Input'
+import InputRegister from '../Small-Components/InputRegister'
 
-const One = () => {
+const One = (card) => {
          const { id }  = useParams()  
-          const [ oneData, setoneData ] = useState([])
+
+   const [title, setTitle ] = useState('')
+   const [url, setUrl] = useState('')
+   const [description, setDescription] = useState('')
+ const [ oneData, setoneData ] = useState([])
+
+     const update = async (id) => {
+            await fetch(`${process.env.REACT_APP_BACKEND_URL}/update/${oneData.id}`,{
+               method: 'PUT',
+               body: JSON.stringify({
+                    title: title,
+                    url: url,
+                    description: description
+               }),
+               headers: {
+				'Content-Type': 'application/json',
+			},
+          })
+          	.then((response) => {
+				if (!response) {
+					throw Error(response.statusText);
+				}
+                    else{
+                         response.json();
+                         toast.configure()
+                    const registerOK = () => {
+                          toast.success('Your registration has been successfully registered', {
+                               autoClose: 3000
+                          })
+                         }     
+                         registerOK()
+                          }
+                    			})
+			.catch((error) => {
+				console.log('Looks like there was an error: \n', error);
+			});
+     }
    
        //testing timeago
 
-  useEffect( () =>Links(setoneData) ,[Links()])
+  useEffect( () =>Links(setoneData) ,[])
 
 const Links = async(setoneData) => {
      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getOne/${id}`)
      const data = await response.json()
      console.log(data[0])
      setoneData(data[0])}
+     console.log('esto es el oneData',oneData)
 
 return (
      <>
      <div className="update_one">
-          <div className="formik">
-                <Formik {...formikUpdate}>
-             {formik => (
-                  <div className="update_section">
-                    
-               <Form>
-                    <Input label='Title' name="title" placeholder={oneData.title} className="input_one"/>
-                    {/* <p>{oneData.url}</p> */}
-                    <Input label="Url" name="url" placeholder={oneData.url} className="input_one"/>
-                    <Input label="Description" name="description" placeholder={oneData.description} className="input_one"/>
-                          <button type="submit" className="update_btn">Update</button>
-               </Form>
-                    
+          <div className="formik">               
+               <div className="update_section">
+                 
+                         <InputRegister label='Title' name="title" placeholder={oneData.title} 
+                         onChange={(event) => setTitle(event.target.value)} className="input_one"/>
+                
+                    <InputRegister label="Url" type="url" name="url" placeholder={oneData.url}   
+                    onChange={(event) => setUrl(event.target.value)}className="input_one"/>
+                    <InputRegister label="Description" name="description" placeholder={oneData.description} 
+                    onChange = {(event) => setDescription(event.target.value)}className="input_one"/>
+                         {/* <div className="button">
+                                 <UpdateFunctio id={oneData.id}/>
+                         </div> */}
+                         <button onClick={(event) => {update(event)}}
+                                   className="btn btn-block btn-success"
+                         
+                         >
+                              <NavLink to="/eachUser">
+                                   Update
+                              </NavLink>
+                              </button>
+                       
                </div>
-             )}
-               
-        </Formik>
           </div>
        
           
