bdac5a2fcd0ddd370bebfb66fa885f5e5f61d6f0
Angel <araozangel842@gmail.com>
2021-07-31 17:25:12 +0300

    Models and saving new image

--- backend_MongoDB/src/controllers/image.js
+++ backend_MongoDB/src/controllers/image.js
@@ -2,23 +2,40 @@ const ctrl = {};
 const path = require('path');
 const { randomName } = require('../helpers/libs');
 const fs = require('fs-extra');
+const { Image } = require('../models/index')
 
 ctrl.index = (req, res) => {
 	res.send('Primera ruta');
 };
 ctrl.create = async (req, res) => {
+     console.log(req.file)
 	const imgUrl = randomName();
 	console.log(imgUrl);
-	const imgTempPath = req.file.path;
-	console.log(imgTempPath);
+	const imgTemporalPath = req.file.path;
+	console.log(imgTemporalPath);
 	const ext = path.extname(req.file.originalname).toLocaleLowerCase();
-	const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
-	console.log(targetPath);
+	const targetSavedPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+	console.log(targetSavedPath);
 
 	if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-		await fs.copy(imgTempPath, targetPath);
-	}
-	res.send('Works');
+		await fs.copy(imgTemporalPath, targetSavedPath)
+                      .then(() => console.log('Success...!!!'))
+                      .catch(err => console.error(err));
+            const newImage = new Image({
+                          title: req.body.title,
+                          filename: imgUrl + ext,
+                          description: req.body.description
+                    })
+          
+          // console.log(req.body);
+          const ImageSaved = await newImage.save()
+          console.log(newImage);
+         
+	}else{
+          console.log('Nop..!!');
+           res.status(403).send('Has a mistake')
+     }
+     res.send('Works');
 };
 ctrl.like = (req, res) => {
 	res.send('Like side');
--- backend_MongoDB/src/models/image.js
+++ backend_MongoDB/src/models/image.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const  Schema  = mongoose.Schema
+const path = require('path')
 
 const imageSchema = new Schema({
-    image: String
+    title : {type: String},
+    filename : {type: String},
+    description : {type: String},
+    timestamp: {type: Date, default: Date.now()}
 
 })
 
+imageSchema.virtual('uniqueId')
+        .get(function () {
+            return this.filename.replace(path.extname(this.filename), '')
+        })
 
-const Image = mongoose.model('Image', imageSchema, 'imgs')
+module.exports = mongoose.model('Image', imageSchema, 'imgs')
 
-module.exports = Image
+
+// const Image = mongoose.model('Image', imageSchema, 'imgs')
+
+// module.exports = Image
