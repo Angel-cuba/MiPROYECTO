a876360c404d040983b9fca2668b3fadbbdd04ad
Angel <araozangel842@gmail.com>
2021-08-03 00:38:47 +0300

    Changing ways of to do server and app

--- backend_MongoDB/src/app.js
+++ backend_MongoDB/src/app.js
@@ -1,11 +1,58 @@
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 const express = require('express');
+const errorhandler = require('errorhandler');
+const cors = require('cors');
+// const Upload  = require('./multer')
 
-const server = require('./server')
+const routes = require('./routes/routes');
 
-const app = server(express())
-require('./database/db')
- 
+//El setMaxListeners()
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+// increase the limit
+myEmitter.setMaxListeners(5);
+for(let i = 0; i < 5; i++) {
+  myEmitter.on('event', _ => console.log(i));
+}
+myEmitter.emit('event');
 
-app.listen(app.get('port'), () => {
-     console.log('Server on port ', app.get('port'))
-})
+module.exports = (app) => {
+	app.set('port', process.env.PORT || 3002);
+
+	app.use(function (req, res, next) {
+		res.header({ 'Access-Control-Allow-Headers': '*' });
+		next();
+	});
+	let options = {
+		origin: '*',
+		method: 'POST, GET, PUT, DELETE',
+	};
+
+	app.use(cors(options));
+
+	//middlewares
+	app.use(morgan('dev'));
+	app.use(multer({ dest: path.join(__dirname, '../public/upload/temp') }).single('image'));
+	//  app.use(Upload)
+	// Para recibir las imágenes que vienen del formulario
+	app.use(express.urlencoded({ extended: false }));
+	//para manejar likes en las fotos o comentarios
+	app.use(express.json());
+
+	//routes
+	routes(app);
+
+	//static files
+	// app.use('public', express.static(path.join(__dirname, '../public')));
+     app.use(express.static(`${__dirname}/public`))
+
+	//errorhandler
+	if ('development' === app.get('env')) {
+		app.use(errorhandler);
+	}
+
+	return app;
+};
--- backend_MongoDB/src/server.js
+++ backend_MongoDB/src/server.js
@@ -1,57 +1,10 @@
-const morgan = require('morgan');
-const multer = require('multer');
-const path = require('path');
 const express = require('express');
-const errorhandler = require('errorhandler');
-const cors = require('cors');
-// const Upload  = require('./multer')
+const server = require('./app')
+const app = server(express())
 
-const routes = require('./routes/routes');
+require('./database/db')
+ 
 
-//El setMaxListeners()
-const EventEmitter = require('events');
-class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
-// increase the limit
-myEmitter.setMaxListeners(5);
-for(let i = 0; i < 5; i++) {
-  myEmitter.on('event', _ => console.log(i));
-}
-myEmitter.emit('event');
-
-module.exports = (app) => {
-	app.set('port', process.env.PORT || 3002);
-
-	app.use(function (req, res, next) {
-		res.header({ 'Access-Control-Allow-Headers': '*' });
-		next();
-	});
-	let options = {
-		origin: '*',
-		method: 'POST, GET, PUT, DELETE',
-	};
-
-	app.use(cors(options));
-
-	//middlewares
-	app.use(morgan('dev'));
-	app.use(multer({ dest: path.join(__dirname, '../public/upload/temp') }).single('image'));
-	//  app.use(Upload)
-	// Para recibir las imágenes que vienen del formulario
-	app.use(express.urlencoded({ extended: false }));
-	//para manejar likes en las fotos o comentarios
-	app.use(express.json());
-
-	//routes
-	routes(app);
-
-	//static files
-	app.use('public', express.static(path.join(__dirname, '../public')));
-
-	//errorhandler
-	if ('development' === app.get('env')) {
-		app.use(errorhandler);
-	}
-
-	return app;
-};
+app.listen(app.get('port'), () => {
+     console.log('Server on port ', app.get('port'))
+})
