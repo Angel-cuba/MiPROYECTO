71b52d655c2738f36bae278ed66dbf07194829a0
Angel <araozangel842@gmail.com>
2021-08-07 15:29:18 +0300

    Using and changing variables from .env

--- backend_MongoDB/src/database/db.js
+++ backend_MongoDB/src/database/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const {database}  =require('../keytoserver/keys')
-require('dotenv').config()
 
 mongoose.connect(database.URI, { 
      useNewUrlParser : true,
