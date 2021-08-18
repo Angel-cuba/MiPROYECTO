ef41700aa9fb78a483a1c90ebd7f1c68fd577ce8
Angel <araozangel842@gmail.com>
2021-08-18 21:36:54 +0300

    Image component view

--- frontend/src/readImages/Images.js
+++ frontend/src/readImages/Images.js
@@ -1,16 +1,20 @@
-export const Images = (image) => {
+import { DeleteImage } from "../Small-Components/ImageDelete"
+export const Images = ({image}) => {
 
      return (
           <>
-               <div className="Images">
-                    <h1>{image.title}</h1>
-                    <div className="image_body">
-                         <img src={image.filename} alt=""/>
+                   <div className="images">
+                         <div className="images_card">
+                              <div className="texto">
+                                   <h1>{image.title}</h1>
+                                   <h3>{image.description}</h3>
+                              </div>
+                              <div className="img">
+                              <img src={image.imageURL} alt=""/>
                     </div>
-                    <div className="image_comment">
-                         <p>{image.description}</p>
+                         <DeleteImage id={image._id}/>
                     </div>
                </div>
-          </>
-     )
+     </>
+               )
 }
