dcc73563718594fc2aada218b13949318c875518
Angel <araozangel842@gmail.com>
2021-08-18 21:34:17 +0300

    Clean component, recieving props(data)

--- frontend/src/readImages/AllImages.js
+++ frontend/src/readImages/AllImages.js
@@ -1,84 +1,32 @@
-import { useEffect, useState } from "react"
 import "../components/pages/css/blog.css"
-import { DeleteImage } from "../Small-Components/ImageDelete"
+import { Images } from "./Images";
 
- import { toast } from 'react-toastify';
- import 'react-toastify/dist/ReactToastify.css';
-
-export const AllImages = () => {
-const [dataImages, setdataImages] = useState([])
-
-
- useEffect( () => {
-      Image(setdataImages)
- }, [])
-
-
+export const AllImages = ({dataImages, search}) => {
+   console.log(search)
+  const FilterImages = dataImages.filter((image) => image.title.toLowerCase().includes(search.toLowerCase()))
 return (
      <>
      {
           (dataImages.length) ? ( 
-           <div className="AllImages">
-               {dataImages.map(image => (
-                    <div key={image._id} className="images">
-                         <div className="images_card">
-                              
-                              <div className="texto">
-                              <h1>{image.title}</h1>
-                              <h3>{image.description}</h3>
-                             
-                         </div>
-
-                              <div className="img">
-                              <img src={image.imageURL} alt=""/>
-                         </div>
-                              <DeleteImage id={image.id}/>
-                         
-                         </div>
-                         
-
-                    </div>
-               ))}
-     </div>
-     ) 
-          : 
+         <div className="AllImages">
+                   { 
+          FilterImages.map((image, index) =>
+           ( <Images image={image} key={index}/> ))
+                     }
+          </div>
+                                   ) 
+                          : 
 
          (
-               <div class="nothing-to-show">
+               <div className="nothing-to-show">
                     <div className="content">
                          <h1>Sorry, <br/> there is nothing to show<br/>😫</h1>
                     </div>
-               
-          </div>
+                </div>
           )
  
      }
      </>
 )
 }
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// async function something() {
-//     console.log("this might take some time....");
-
-//     await delay(5000);
-//     console.log("done!")
-// }
-const Image = async(setdataImages) => {
-      toast.configure();
- const response = await fetch(`${process.env.REACT_APP_BACKEND_MONGODB_URL}`)
- const data = await response.json()
- console.log(data)
- const Warning = () => {
- 				toast.info('Time of responses', {
-					position: toast.POSITION.TOP_CENTER,
- 					autoClose: 3000
- 				})
-			}
-
-
- await delay(Warning(),3000);
- console.log('Now.....!')
- setdataImages(data)
- 
-}
