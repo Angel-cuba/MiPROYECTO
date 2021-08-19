b86b4afef68eb93775edea15f6623ef2f27dc02d
Angel <araozangel842@gmail.com>
2021-08-19 20:05:50 +0300

    Change of regular expression

--- frontend/src/lib/validateForms/validateLink.js
+++ frontend/src/lib/validateForms/validateLink.js
@@ -6,14 +6,15 @@ export const validateLink = yup.object().shape({
                     .required('This field is required'),
      url : yup.string()
                     .url('Must be a valid url')
-                    .matches(/^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm, 'This address has some invalid characters')
+                    .matches(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm, 'This address has some invalid characters')
                     .required('This field is required'),
      description : yup.string()
-                    .min(10, 'Plzz, try to give somthing to show...')
+                    .min(10, 'Plzz, try to give something to show...')
                     .max(20, 'Upss, seems like is too much')
+                    .required('This field is required')
                
 })
-
+.required('This field is required')
 export const initialValuesLink = {
      title : '',
      url: '',
