6131990464b1e4c8b11752955d617f82c4daaac1
Angel <araozangel842@gmail.com>
2021-08-20 21:38:40 +0300

    Change
    createPool instead createConnection

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const dbConnection = mysql.createConnection({
+const dbConnection = mysql.createPool({
      host: process.env.DB_HOST,
      user: process.env.DB_USER,
      password: process.env.DB_PASSWORD,
@@ -9,19 +9,20 @@ const dbConnection = mysql.createConnection({
      port: process.env.DB_PORT
 });
 
-dbConnection.connect((error) => {
+dbConnection.getConnection((error) => {
     if (error) {
         if (error.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
+            console.log('Database connection was closed.');
         }
         if (error.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.');
+            console.log('Database has too many connections.');
         }
         if (error.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.');
+            console.log('Database connection was refused.');
         }
+        throw error;
     } else {
-        console.log('Database is already---->', dbConnection.state);
+        console.log('Database is already connected---->', dbConnection.config.acquireTimeout);
      //    console.log('Database connected',dbConnection);
 
     }
