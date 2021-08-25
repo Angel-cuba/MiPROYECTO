0a3d47e9ab770adf8d3699e4e7d63a8001bdbcc3
Angel <araozangel842@gmail.com>
2021-08-25 20:51:52 +0300

    Using Sweetalert2 in React component

--- frontend/src/lib/FormikLink.js
+++ frontend/src/lib/FormikLink.js
@@ -1,11 +1,16 @@
+import { validateLink, initialValuesLink } from './validateForms/validateLink'
+
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { array } from '../Small-Components/Swal'
+const MySwal = withReactContent(Swal);
 
 //Using SweetAlert2
-import Swal from 'sweetalert2'
+// import Swal from 'sweetalert2'
 
 
-import { validateLink, initialValuesLink } from './validateForms/validateLink'
 
 
 export const FormikLink = {
@@ -24,46 +29,30 @@ export const FormikLink = {
                          description: values.description
                     }),
 			});
-			// if(response){
-			// 	Swal.fire({
-			// 	title: 'Succefully!',
-			// 	text: 'Your link went to DB now!',
-			// 	icon: 'success',
-
-			// 	showClass: {
-			// 	popup: 'animate__animated animate__fadeInDown'
-			// 	},
-			// 	hideClass: {
-			// 	popup: 'animate__animated animate__fadeOutUp'
-			// 	}
-			// })
-			// }
+		
 			const json = await response.json();
 			console.log(json);
-			//  toast.configure()
-			//  toast.success('Your comment has been added.')
+		
+		MySwal.fire({
+			title: array.title,
+			footer: array.footer,
+			background:  array.background,
+			icon: array.icon,
+			timer: array.timer,
+			timerProgressBar: array.timerProgressBar
+
+})
+.then(() => {		
+  return MySwal.fire({
+	  				title: array.segundo_title,
+					timer: array.segundo_timer
+					  })
+           			.then(()=> setTimeout(()=> window.location.reload()))
+})
 			
 		} catch (error) {
 			console.log(error);
 		}
-          
-		// history.push('/eachUser');
-			
-		setTimeout( Swal.fire({
-				title: 'Succefully!',
-				text: 'Your link went to DB now!',
-				timer: 3000,
-				icon: 'success',
-				
-				timerProgressBar: true,
-				showClass: {
-				popup: 'animate__animated animate__fadeInDown'
-				},
-				hideClass: {
-				popup: 'animate__animated animate__fadeOutUp'
-				}
-			}),4000 )
-		window.location.reload();
           window.location.href('/eachUser')
 
      },
