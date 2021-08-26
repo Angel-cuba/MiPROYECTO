e5a4933286ae23a90e12e524b550e9ab88fe2453
Angel <araozangel842@gmail.com>
2021-08-26 18:43:29 +0300

    Animation to wait for data

---
+++ frontend/src/Small-Components/Spinners/ImgSpinner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const ImgSpinner = () => {
+     return (
+          <>
+          <div className="Spinner">
+                    <h1 data-text="Waiting...">
+                         Waiting...</h1>
+
+<div className="imgspinner">
+                         <div className="inside">
+                    </div>
+               </div>
+          </div>
+               
+          </>
+     )
+}
