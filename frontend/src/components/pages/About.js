a1f4993bb9121000fcdb7399d599c8b9f6e2591f
Angel <araozangel842@gmail.com>
2021-08-30 14:12:41 +0300

    Working with Spinner animation

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -1,9 +1,11 @@
+import { DownSpinner } from '../../Small-Components/Spinners/DownSpinner'
 import './css/about.css'
 
 const About = () => {
    return (
           <div className="pages">
           <h1>La parte de About</h1>
+          <DownSpinner/>
      </div>
    )
 }
