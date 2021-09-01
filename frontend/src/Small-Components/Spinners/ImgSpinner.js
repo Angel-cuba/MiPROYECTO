e294ae0e689d777d6235ee60fbc91126724b02e3
Angel <araozangel842@gmail.com>
2021-09-01 19:23:54 +0300

    New BarSpinner and new folder for spinner css

--- frontend/src/Small-Components/Spinners/ImgSpinner.js
+++ frontend/src/Small-Components/Spinners/ImgSpinner.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../../Small-Components/css/DownSpinner.css"
 
 export const ImgSpinner = () => {
      return (
--- frontend/src/components/pages/Team.js
+++ frontend/src/components/pages/Team.js
@@ -1,8 +1,10 @@
+import { BarSpinner } from "../../Small-Components/Spinners/BarSpinner"
 
 const Team  = () => {
 return (
       <div className="pages">
       <h1>La parte de Teams</h1>
+      <BarSpinner/>
  </div>
 )
 }
