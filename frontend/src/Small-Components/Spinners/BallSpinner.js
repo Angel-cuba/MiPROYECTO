69b783c6a33a43a9d2521d5467ea3e480f1da782
Angel <araozangel842@gmail.com>
2021-09-08 21:31:35 +0300

    New spinners

--- frontend/src/Small-Components/Spinners/BallSpinner.js
+++ frontend/src/Small-Components/Spinners/BallSpinner.js
@@ -5,12 +5,12 @@ export const BallSpinner = () => {
      return (
           <>
                <div className="balls">
-                    <div class="small-ball ball-1"></div>
-                    <div class="small-ball ball-2"></div>
-                    <div class="small-ball ball-3"></div>
-                    <div class="small-ball ball-4"></div>
-                    <div class="small-ball ball-5"></div>
-                    <div class="small-ball ball-6"></div>
+                    <div className="small-ball ball-1"></div>
+                    <div className="small-ball ball-2"></div>
+                    <div className="small-ball ball-3"></div>
+                    <div className="small-ball ball-4"></div>
+                    <div className="small-ball ball-5"></div>
+                    <div className="small-ball ball-6"></div>
                     </div>   
           </>
      )
---
+++ frontend/src/Small-Components/Spinners/SmallSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/SmallSpinner.css'
+
+export const SmallSpinner = () => {
+     return (
+          <>
+            <div className="spinn">
+                 <div className="round">
+                      +
+                 </div>
+                 </div>   
+          </>
+     )
+}
